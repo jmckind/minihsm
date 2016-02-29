0a5516a917a6743c26c9f2ea788a0014b4bb321b
John McKenzie <jmckind@gmail.com>
2016-02-29 13:51:19 -0600

    Initial commit

---
+++ minihsm/server.py
@@ -0,0 +1,69 @@
+
+import falcon
+import logging
+from yubikey import GhettoYubiHSM
+
+FORMAT = '%(asctime)-15s %(levelname)s %(funcName)s(%(lineno)d): %(message)s'
+logging.basicConfig(format=FORMAT, level=logging.DEBUG)
+LOG = logging.getLogger(__name__)
+
+class EncryptResource:
+    """
+    Resource to encrypt plaintext and return the ciphertext.
+    """
+
+    def on_post(self, req, resp):
+        """
+        Handle HTTP POST requests.
+        Args:
+            req: the request object.
+            resp: the response object.
+        """
+
+        pt = req.stream.read()
+        LOG.debug("plaintext: %s" % pt)
+
+        ct = self.encrypt(pt)
+        LOG.debug("ciphertext: %s" % ct)
+
+        resp.body = ct
+
+    def encrypt(self, pt):
+        """
+        This method will "encrypt" the provided plaintext value.
+        """
+        hsm = GhettoYubiHSM()
+        return hsm.encrypt(pt)
+
+class DecryptResource:
+    """
+    Resource to decrypt ciphertext and return the plaintext.
+    """
+
+    def on_post(self, req, resp):
+        """
+        Handle HTTP POST requests.
+        Args:
+            req: the request object.
+            resp: the response object.
+        """
+
+        ct = req.stream.read()
+        LOG.debug("ciphertext: %s" % ct)
+
+        pt = self.decrypt(ct)
+        LOG.debug("plaintext: %s" % pt)
+
+        resp.body = pt
+
+    def decrypt(self, ct):
+        """
+        This method will "decrypt" the provided ciphertext value.
+        """
+        hsm = GhettoYubiHSM()
+        return hsm.decrypt(ct)
+
+
+app = falcon.API()
+app.add_route('/decrypt', DecryptResource())
+app.add_route('/encrypt', EncryptResource())
---
+++ minihsm/yubukey.py
@@ -0,0 +1,48 @@
+import os
+import base64
+from Crypto.Cipher import AES
+import yubico
+
+class GhettoYubiHSM(object):
+    # the block size for the cipher object; must be 16 per FIPS-197
+    BLOCK_SIZE = 16
+    PADDING = b'\x00'
+
+    def __init__(self):
+        self._yubikey = yubico.find_yubikey(debug=False)
+
+    def _pad(self, s):
+        return s + (self.BLOCK_SIZE - len(s) % self.BLOCK_SIZE) * self.PADDING
+
+    def _get_key(self, salt=None):
+        if salt is None:
+            salt = os.urandom(self.BLOCK_SIZE)
+        elif len(salt) != self.BLOCK_SIZE:
+            raise ValueError('Salt must be exactly %d bytes in length' % self.BLOCK_SIZE)
+
+        key = self._pad(self._yubikey.challenge_response(salt, slot=2))[:self.BLOCK_SIZE]
+
+        return salt, key
+
+    def encrypt(self, s):
+        salt, key = self._get_key()
+
+        cipher = AES.new(key)
+        encrypted_data = cipher.encrypt(self._pad(s))
+
+        return base64.b64encode('%s%s' % (salt, encrypted_data))
+
+    def decrypt(self, s):
+        try:
+            decoded_string = base64.b64decode(s)
+            salt = decoded_string[:self.BLOCK_SIZE]
+            data = decoded_string[self.BLOCK_SIZE:]
+
+            salt, key = self._get_key(salt)
+
+            cipher = AES.new(key)
+            decoded_data = cipher.decrypt(data).rstrip(self.PADDING)
+
+            return decoded_data
+        except:
+            raise ValueError('Not a valid encrypted string')
